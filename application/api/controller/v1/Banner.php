ee78d2d0e91079862a7cc2ab52e672df9e3bf955
rain.chen <644471470@qq.com>
2019-02-25 23:50:07 +0800

    4-2 Banner接口定义及自定义控制器多级目录

---
+++ application/api/controller/v1/Banner.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/2/25
+ * Time: 23:40
+ */
+
+namespace app\api\controller\v1;
+
+
+class Banner
+{
+    public function getBanner($id){
+        echo 111;
+    }
+}
