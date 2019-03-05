5bd9a4450644e296bdf3d0a47db3c47d1450e4ac
rain.chen <644471470@qq.com>
2019-03-05 22:23:06 +0800

    6-7 在全局异常处理中加入日志记录

--- application/lib/exception/ExceptionHandler.php
+++ application/lib/exception/ExceptionHandler.php
@@ -11,6 +11,7 @@ namespace app\lib\exception;
 
 use Exception;
 use think\exception\Handle;
+use think\Log;
 use think\Request;
 
 class ExceptionHandler extends  Handle
@@ -20,22 +21,33 @@ class ExceptionHandler extends  Handle
     private $errorCode;
     public function render(Exception $e)
     {
-        if($e instanceof BaseException){
+        if ($e instanceof BaseException) {
             //如果是自定义异常
             $this->code = $e->code;
             $this->msg = $e->msg;
-            $this->errorCode =$e->errorCode;
-        }else{
+            $this->errorCode = $e->errorCode;
+        } else {
             $this->code = 500;
             $this->msg = '服务器内部错误，就不告诉你';
             $this->errorCode = 999;
+            $this->recordErrorLog($e);//写入日志
         }
         $request = Request::instance();
         $result = [
-            'msg'=>$this->msg,
-            'errorCode'=>$this->errorCode,
-            'request_rul'=>$request->url()
+            'msg' => $this->msg,
+            'errorCode' => $this->errorCode,
+            'request_rul' => $request->url()
         ];
-        return json($result,$this->code);
+        return json($result, $this->code);
+    }
+        //自定义日志
+    private function recordErrorLog(Exception $e){
+        Log::init([
+            'type'  =>  'File',
+            'path'  =>  LOG_PATH,
+            // 日志记录级别，使用数组表示
+            'level' => ['error'],
+        ]);
+        Log::record($e->getMessage(),'error');
     }
 }
