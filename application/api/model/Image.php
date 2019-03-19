f4ec0ead304098889c529e57270635a4c6d51332
rain.chen <rain.chen@ouzudz.com>
2019-03-19 17:35:32 +0800

    8-7 读取器的巧妙应用

--- application/api/model/Image.php
+++ application/api/model/Image.php
@@ -19,5 +19,17 @@ class Image extends Model {
 
     //隐藏不需要显示的字段
     protected $hidden = ['id', 'from', 'update_time', 'delete_time'];
-
+   /**
+    * 获取器
+    * @param type $value 字段值
+    * @param type $data 整个表的数据
+    * @return type
+    */
+    public function getUrlAttr($value,$data) {
+        $finalUrl = $value;
+        if($data['from'] ==1){
+            $finalUrl = config('setting.img_prefix').$finalUrl;
+        }
+        return $finalUrl;
+    }
 }
