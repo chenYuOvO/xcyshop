45b98d9f6d2fe1e9d4cee29990b0336246b237f2
rain.chen <rain.chen@ouzudz.com>
2019-03-21 14:40:43 +0800

    8-11 一对一关系解析

---
+++ application/api/model/Theme.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace app\api\model;
+
+use think\Model;
+
+/**
+ * Description of Theme
+ *
+ * @author admin
+ */
+class Theme extends BaseModel {
+
+    public function topicImg() {
+        return $this->belongsTo('Image', 'topic_img_id', 'id');
+    }
+
+    public function headImg() {
+        return $this->belongsTo('Image', 'head_img_id', 'id');
+    }
+
+}
