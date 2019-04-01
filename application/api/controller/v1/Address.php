d72b493ccbf6909ae1eb86d9f288f0f4ad17b6de
rain.chen <rain.chen@ouzudz.com>
2019-04-01 11:06:43 +0800

    9-12 用户收货地址-----通过令牌获取用户标识

---
+++ application/api/controller/v1/Address.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace app\api\controller\v1;
+
+/**
+ * Description of Address
+ *
+ * @author admin
+ */
+class Address {
+
+    public function createOrUpdateAddress() {
+        
+    }
+
+}
---
+++ application/api/validate/AddressNew.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace app\api\validate;
+
+/**
+ * Description of AddressNew
+ *
+ * @author admin
+ */
+class AddressNew extends BaseValidate{
+    protected $rule = [
+        'name'=>'require|isNotEmpty',
+        'mobile'=>'require|isMobile',
+        'province'=>'require|isNotEmpty',
+        'city'=>'require|isNotEmpty',
+        'country'=>'require|isNotEmpty',
+        'detail'=>'require|isNotEmpty',
+    ];
+}
