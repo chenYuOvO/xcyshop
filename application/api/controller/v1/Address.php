e5e2eb19d3e888c0aa7185dbafbf87e3c16667cf
rain.chen <rain.chen@ouzudz.com>
2019-04-01 14:10:40 +0800

    9-13 用户收货地址-----面向对象的方式封装获取UID方法

--- application/api/controller/v1/Address.php
+++ application/api/controller/v1/Address.php
@@ -8,6 +8,9 @@
 
 namespace app\api\controller\v1;
 
+use app\api\validate\AddressNew;
+use app\api\service\Token as TokenService;
+
 /**
  * Description of Address
  *
@@ -16,7 +19,12 @@ namespace app\api\controller\v1;
 class Address {
 
     public function createOrUpdateAddress() {
-        
+        (new AddressNew())->goCheck();
+        //获取用户token
+        //根据用户的uid找到用户，如果未找到，抛出异常
+        //获取从客户端传过来的地址信息
+        //根据用户地址信息是否存在，从而判断是添加还是更新
+        $uid = TokenService::getCurrentUid();
     }
 
 }
--- application/api/service/Token.php
+++ application/api/service/Token.php
@@ -8,6 +8,10 @@
 
 namespace app\api\service;
 
+use think\Cache;
+use think\Request;
+use app\lib\exception\TokenException;
+
 /**
  * Description of Token
  *
@@ -25,4 +29,35 @@ class Token {
         return md5($randChars . $timestamp . $salt);
     }
 
+    /**
+     * 获取token缓存中的某个值
+     * @param type $key
+     * @return type
+     * @throws TokenException
+     * @throws Exception
+     */
+    public static function getCurrentTokenVar($key) {
+        $token = Request::instance()->header('token');
+        $vars = Cache::get($token);
+        if (!$vars) {
+            throw new TokenException();
+        } else {
+            if (!is_array($vars)) {
+                $vars = json_decode($vars, TRUE);
+            }
+            if (array_key_exists($key, $vars)) {
+                return $vars[$key];
+            } else {
+                throw new Exception('尝试获取的Token不存在');
+            }
+        }
+    }
+    /**
+     * 获取用户uid
+     * @return type
+     */
+    public static function getCurrentUid() {
+        $uid = self::getCurrentTokenVar('uid');
+        return $uid;
+    }
 }
