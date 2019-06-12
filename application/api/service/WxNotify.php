7e75c237cbbf8fec84e2968a97d281aee0f03bcf
rain.chen <rain.chen@ouzudz.com>
2019-06-12 16:23:33 +0800

    10-30 支付回调通知处理 二

--- application/api/service/WxNotify.php
+++ application/api/service/WxNotify.php
@@ -8,6 +8,15 @@
 
 namespace app\api\service;
 
+use app\api\model\Order;
+use app\api\model\Product;
+use app\api\service\Order as OrderService;
+use app\lib\enum\OrderStatusEnum;
+use app\lib\order\OrderStatus;
+use think\Db;
+use think\Exception;
+use think\Loader;
+use think\Log;
 use think\Loader;
 
 Loader::import('WxPay.WxPay', EXTEND_PATH, '.Api.php');
@@ -20,7 +29,56 @@ Loader::import('WxPay.WxPay', EXTEND_PATH, '.Api.php');
 class WxNotify extends \WxPayNotify {
 
     public function NotifyProcess($objData, $config, &$msg) {
-        
+
+        $data = $objData->GetValues();
+        if ($data['result_code'] == 'SUCCESS') {
+            $orderNo = $data['out_trade_no'];
+            Db::startTrans();
+            try {
+                $order = Order::where('order_no', '=', $orderNo)->lock(true)->find();
+                if ($order->status == 1) {
+                    $service = new OrderService();
+                    $stockStatus = $service->checkOrderStock($order->id);//检测库存
+                    if ($stockStatus['pass']) {
+                        //库存充足
+                        $this->updateOrderStatus($order->id, true);
+                        $this->reduceStock($stockStatus);
+                    } else {
+                        //库存不充足
+                        $this->updateOrderStatus($order->id, false);
+                    }
+                }
+                Db::commit();
+            } catch (Exception $ex) {
+                Db::rollback();
+                Log::error($ex);
+                // 如果出现异常，向微信返回false，请求重新发送通知
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 减库存
+     * @param type $status
+     */
+    private function reduceStock($status) {
+        foreach ($status['pStatusArray'] as $singlePStatus) {
+            Product::where('id', '=', $singlePStatus['id'])
+                    ->setDec('stock', $singlePStatus['count']);
+        }
+    }
+
+    /**
+     * 更改订单状态
+     * @param type $orderID
+     * @param type $success
+     */
+    private function updateOrderStatus($orderID, $success) {
+        $status = $success ? OrderStatusEnum::PAID : OrderStatusEnum::PAID_BUT_OUT_OF;
+        Order::where('id', '=', $orderID)
+                ->update(['status' => $status]);
     }
 
 }
